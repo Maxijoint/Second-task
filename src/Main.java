8102ce6dfab1c9c7066b74bcccc324ff6f0c5eb7
Maxijoint <maxijoint@yandex.ru>
2024-01-30 17:23:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int price = 5000; // Стоимость билета
+        int miles = 50; //  Количество рублей для одной бонусной мили
+
+        int bonus;
+
+        bonus = price / miles; // Формула подсчета бонуса
+
+        System.out.println("Количество бонусов" + bonus);
 
     }
 }
